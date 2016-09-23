f14220ed57bdaa9f0942b3fe5f9f301333577692
Sandro Alvarez <sandroalvarez@yahoo.com>
2016-09-22 18:57:23 -0700

    add Client class with necessary methods and passing tests

---
+++ src/main/java/Client.java
@@ -0,0 +1,123 @@
+import java.util.List;
+import java.util.ArrayList;
+import org.sql2o.*;
+
+public class Client {
+  private String name;
+  private String preferences;
+  private int age;
+  private int id;
+  private int stylistId;
+
+  public Client(String name, String preferences, int age, int stylistId) {
+    this.name = name;
+    this.preferences = preferences;
+    this.age = age;
+    this.stylistId = stylistId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getPreferences() {
+    return preferences;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public int getStylistId() {
+    return stylistId;
+  }
+
+  public static List<Client> all() {
+    String sql = "SELECT id, name, preferences, age, stylistId FROM clients";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Client.class);
+    }
+  }
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO clients (name, preferences, age, stylistId) VALUES (:name, :preferences, :age, :stylistId)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .addParameter("preferences", this.preferences)
+        .addParameter("age", this.age)
+        .addParameter("stylistId", this.stylistId)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  public static Client find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM clients where id=:id";
+      Client media = con.createQuery(sql)
+      .addParameter("id", id)
+      .executeAndFetchFirst(Client.class);
+    return media;
+    }
+  }
+
+  public void updateName(String name) {
+    this.name = name;
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE clients SET name = :name WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("name", name)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public void updatePreferences(String preferences) {
+    this.preferences = preferences;
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE clients SET preferences = :preferences WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("preferences", preferences)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public void updateAge(int age) {
+    this.age = age;
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "UPDATE clients SET age = :age WHERE id = :id";
+      con.createQuery(sql)
+        .addParameter("age", age)
+        .addParameter("id", id)
+        .executeUpdate();
+    }
+  }
+
+  public void delete() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM clients WHERE id = :id;";
+      con.createQuery(sql)
+      .addParameter("id", id)
+      .executeUpdate();
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherClient) {
+    if(!(otherClient instanceof Client)) {
+      return false;
+    } else {
+      Client newClient = (Client) otherClient;
+      return this.getName().equals(newClient.getName()) &&
+             this.getPreferences().equals(newClient.getPreferences()) && this.getAge() == newClient.getAge() &&
+             this.getId() == newClient.getId() &&
+             this.getStylistId() == newClient.getStylistId();
+    }
+  }
+}
---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,109 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ClientTest {
+  private Client firstClient;
+  private Client secondClient;
+
+  @Before
+  public void initialize() {
+    firstClient = new Client("Sandro", "men's cut", 23, 1);
+    secondClient = new Client("Melissa", "women's cut", 33, 1);
+  }
+
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
+
+  @Test
+  public void Client_instantiatesCorrectly_true() {
+    assertEquals(true, firstClient instanceof Client);
+  }
+
+  @Test
+  public void getName_returnsName_String() {
+    assertEquals("Sandro", firstClient.getName());
+  }
+
+  @Test
+  public void getId_returnsId_true() {
+    firstClient.save();
+    assertTrue(firstClient.getId() > 0);
+  }
+
+  @Test
+  public void all_returnsAllInstancesOfClient_true() {
+    firstClient.save();
+    secondClient.save();
+    assertTrue(Client.all().get(0).equals(firstClient));
+    assertTrue(Client.all().get(1).equals(secondClient));
+  }
+
+  @Test
+  public void find_returnsClientWithSameId_secondClient() {
+    firstClient.save();
+    secondClient.save();
+    assertEquals(Client.find(secondClient.getId()), secondClient);
+  }
+
+  @Test
+  public void equals_returnsTrueIfNamesAreTheSame() {
+    Client myClient = new Client("Sandro", "men's cut", 23, 1);
+    assertTrue(firstClient.equals(myClient));
+  }
+
+  @Test
+  public void save_returnsTrueIfNamesAreTheSame() {
+    firstClient.save();
+    assertTrue(Client.all().get(0).equals(firstClient));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    firstClient.save();
+    Client savedClient = Client.all().get(0);
+    assertEquals(firstClient.getId(), savedClient.getId());
+  }
+
+  // @Test
+  // public void save_savesStylistIdIntoDB_true() {
+  //   Stylist myStylist = new Stylist("Chase", "37", "15 years experience");
+  //   myStylist.save();
+  //   Client myClient = new Client("Satchel", "men's cut with face shave", 24, myStylist.getId());
+  //   myClient.save();
+  //   Client savedClient = Client.find(myClient.getId());
+  //   assertEquals(savedClient.getStylistId(), myStylist.getId());
+  // }
+
+  @Test
+  public void updateName_updatesClientName_true() {
+    firstClient.save();
+    firstClient.updateName("Mateo");
+    assertEquals("Mateo", Client.find(firstClient.getId()).getName());
+  }
+
+  @Test
+  public void updatePreferences_updatesClientPreferences_true() {
+    firstClient.save();
+    firstClient.updatePreferences("buzz cut");
+    assertEquals("buzz cut", Client.find(firstClient.getId()).getPreferences());
+  }
+
+  @Test
+  public void updateAge_updatesClientAge_true() {
+    firstClient.save();
+    firstClient.updateAge(26);
+    assertEquals(26, Client.find(firstClient.getId()).getAge());
+  }
+
+  @Test
+  public void delete_deletesClient_true() {
+    firstClient.save();
+    int firstClientId = firstClient.getId();
+    firstClient.delete();
+    assertEquals(null, Client.find(firstClientId));
+  }
+}
